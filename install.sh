79aa8d2b0c284ab134010011bba18c7793010196
Thomas Morris <thomas.imorris@gmail.com>
2014-12-20 11:02:01 +0200

    Run NeoBundle install script from our install script

--- install.sh
+++ install.sh
@@ -11,7 +11,10 @@ our_vimrc=$dir/vimrc
 system_vimrc=~/.vimrc
 backup_path=~/.vimrc-backup
 
-# Add automatic installation of the NeoBundle script - look on github
+# Install NeoBundle
+echo "Installing the NeoBundle package manager for Vim..."
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+echo "Done installing NeoBundle."
 
 # Make a backup of user's vimrc
 if [ -a $system_vimrc ]; then #if vimrc exists
@@ -27,4 +30,4 @@ echo -n "Making a symlink of the new .vimrc file..."
 ln -s $our_vimrc $system_vimrc
 echo "done"
 
-echo "All finished! Boot up vim and give it a spin!"
+echo "All finished! Boot up Vim and give it a spin!"
